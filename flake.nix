a2f88bf289fcbe87ecf349c5e5106951f73ffc10
Valyn Tyler <git@valyntyler.com>
2024-11-16 17:02:09 +0200

    feat: added home manager

--- flake.nix
+++ flake.nix
@@ -3,14 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
       modules = [
         ./hosts/desktop
+        inputs.home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users."valyn" = import ./home.nix;
+        }
       ];
     };
   };
 }
+
