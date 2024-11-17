598b9953507284f80f46daf59405e39e12d7c10b
Valyn Tyler <git@valyntyler.com>
2024-11-17 05:29:13 +0200

    feat: update lazygit loading spinner

--- home/lazygit.nix
+++ home/lazygit.nix
@@ -2,9 +2,23 @@
   programs.lazygit.enable = true;
   programs.lazygit.settings = {
     gui = {
+      # scrolloff
       scrollPastBottom = true;
       scrollOffMargin = 999;
       scrollOffBehaviour = "margin";
+    
+      # spinner
+      spinner.rate = 100;
+      spinner.frames = [ # (find more at https://www.npmjs.com/package/cli-spinners)
+        "←"
+        "↖"
+        "↑"
+        "↗"
+        "→"
+        "↘"
+        "↓"
+        "↙"
+      ];
     };
   };
 }
