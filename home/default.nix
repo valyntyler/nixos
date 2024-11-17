c6acf9ae48d73ceb6c214d5e40434003a4e2ef9f
Valyn Tyler <git@valyntyler.com>
2024-11-17 05:48:55 +0200

    feat: added and configured yazi

--- home/default.nix
+++ home/default.nix
@@ -6,6 +6,7 @@
     ./shell
     ./git.nix
     ./lazygit.nix
+    ./yazi.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
---
+++ home/yazi.nix
@@ -0,0 +1,9 @@
+{
+  programs.yazi = {
+    enable = true;
+    settings.manager = {
+      scrolloff = 200;
+      show_symlink = false;
+    };
+  };
+}
