a379dc8932104dfc4845b3af3eaa5888f989f846
Valyn Tyler <git@valyntyler.com>
2024-11-19 20:08:34 +0200

    feat: added opacity toggle for wezterm

--- home/terminal/wezterm.lua
+++ home/terminal/wezterm.lua
@@ -4,7 +4,33 @@ local config = wezterm.config_builder()
 -- show meaningful characters
 config.front_end = "WebGpu"
 
-config.font = wezterm.font 'JetBrainsMonoNF'
+-- configure colorscheme
 config.color_scheme = 'GruvboxDark'
+
+config.font = wezterm.font 'JetBrainsMonoNF'
+config.font_size = 12.0;
+
+-- toggle background opacity
+wezterm.on("toggle-opacity", function(window, _)
+  local overrides = window:get_config_overrides() or {}
+  if not overrides.window_background_opacity then
+    overrides.window_background_opacity = 0.8
+    overrides.text_background_opacity = 0.8
+  else
+    overrides.window_background_opacity = nil
+    overrides.text_background_opacity = nil
+  end
+  window:set_config_overrides(overrides)
+end)
+
+-- keybinds
+config.keys = {
+  {
+    -- press ctrl+shift+y to toggle transparent background
+    key = 'Y',
+    mods = 'CTRL',
+    action = wezterm.action.EmitEvent 'toggle-opacity',
+  },
+}
 
 return config
