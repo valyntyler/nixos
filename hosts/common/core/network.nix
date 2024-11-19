d0308ac1aeb43a8c3ff1ef0a80fa4d2ac1333bdd
Valyn Tyler <git@valyntyler.com>
2024-11-19 17:01:21 +0200

    feat: configured syncthing

--- hosts/common/core/network.nix
+++ hosts/common/core/network.nix
@@ -6,6 +6,10 @@
     hostName = "nixos-desktop"; # Define your hostname.
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+    # open ports for syncthing
+    firewall.allowedTCPPorts = [ 8384 22000 ];
+    firewall.allowedUDPPorts = [ 22000 21027 ];
+
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
--- hosts/common/core/syncthing.nix
+++ hosts/common/core/syncthing.nix
@@ -1,5 +1,25 @@
 {
   services.syncthing = {
     enable = true;
+    user = "valyn";
+
+    overrideFolders = true;
+    overrideDevices = true;
+
+    configDir = "/home/valyn/.config/syncthing";
+    dataDir = "/home/valyn";
+
+    settings = {
+      folders."obsidian" = {
+        path = "~/obsidian";
+        devices = [ "phone" "raspi" ];
+      };
+
+      devices.phone = { id = "ZM3DTSX-SCONYIW-H52CZ3C-O7QK2EP-QCKU7M3-B2FKLBA-ZQ6AE7D-LNBGNQN"; introducer = true; };
+      devices.raspi = { id = "CKLQUXJ-OZ7376G-SIJJTGS-Q7WKT4Y-UYYH2EE-DEU2PCC-YFRW67Z-NDKOPAN"; introducer = false; };
+    };
   };
+
+  # Don't create default ~/Sync folder
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
 }
