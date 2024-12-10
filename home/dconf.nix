bd59267a18ec1b2c55f52aaccfd7b7fadc446f25
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-10 23:01:25 +0200

    chore: update dconf formatting

--- home/dconf.nix
+++ home/dconf.nix
@@ -23,8 +23,8 @@
       # tell dconf where to look for custom keybindings
       custom-keybindings = [
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
       ];
     };
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
