1d3ed3b3e6c70f7e56ed6ca7bc6a1009cdd1bdbd
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-18 11:38:30 +0200

    feat: set default SE

--- home/browser/firefox.nix
+++ home/browser/firefox.nix
@@ -5,10 +5,12 @@
     profiles."self" = {
       id = 0;
       isDefault = false;
+      search.default = "DuckDuckGo";
     };
     profiles."work" = {
       id = 1;
       isDefault = true;
+      search.default = "DuckDuckGo";
     };
   };
 }
