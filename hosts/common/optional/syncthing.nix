81c10a1b4db32fc3dc7316d57347d9ec18450e8b
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-19 02:06:02 +0200

    feat: added laptop to syncthing devices

--- hosts/common/optional/syncthing.nix
+++ hosts/common/optional/syncthing.nix
@@ -12,11 +12,16 @@
     settings = {
       folders."obsidian" = {
         path = "~/obsidian";
-        devices = [ "phone" "raspi" ];
+        devices = [
+          "phone"
+          "raspi"
+          "laptop"
+        ];
       };
 
       devices.phone = { id = "ZM3DTSX-SCONYIW-H52CZ3C-O7QK2EP-QCKU7M3-B2FKLBA-ZQ6AE7D-LNBGNQN"; introducer = true; };
       devices.raspi = { id = "CKLQUXJ-OZ7376G-SIJJTGS-Q7WKT4Y-UYYH2EE-DEU2PCC-YFRW67Z-NDKOPAN"; introducer = false; };
+      devices.laptop = { id = "ZLFHKY7-Z453X2W-RBC7BBE-F64ZXNP-EBVJBLE-F7L2QPP-MBLSRUH-PHHVJAH"; introducer = true; };
     };
   };
 
