f40f74110b4c1255cfc3c70dbd997ba77f683235
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-19 02:36:42 +0200

    fix: syncthing declaring disabled introducer key

--- hosts/common/optional/syncthing.nix
+++ hosts/common/optional/syncthing.nix
@@ -19,9 +19,9 @@
         ];
       };
 
-      devices.phone = { id = "ZM3DTSX-SCONYIW-H52CZ3C-O7QK2EP-QCKU7M3-B2FKLBA-ZQ6AE7D-LNBGNQN"; introducer = true; };
-      devices.raspi = { id = "CKLQUXJ-OZ7376G-SIJJTGS-Q7WKT4Y-UYYH2EE-DEU2PCC-YFRW67Z-NDKOPAN"; introducer = false; };
-      devices.laptop = { id = "ZLFHKY7-Z453X2W-RBC7BBE-F64ZXNP-EBVJBLE-F7L2QPP-MBLSRUH-PHHVJAH"; introducer = true; };
+      devices.phone = { id = "ZM3DTSX-SCONYIW-H52CZ3C-O7QK2EP-QCKU7M3-B2FKLBA-ZQ6AE7D-LNBGNQN"; };
+      devices.raspi = { id = "CKLQUXJ-OZ7376G-SIJJTGS-Q7WKT4Y-UYYH2EE-DEU2PCC-YFRW67Z-NDKOPAN"; };
+      devices.laptop = { id = "ZLFHKY7-Z453X2W-RBC7BBE-F64ZXNP-EBVJBLE-F7L2QPP-MBLSRUH-PHHVJAH"; };
     };
   };
 
