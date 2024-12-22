95534d9737d49bfaf680f3dd1685d4de7bb8fdeb
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-22 21:03:52 +0200

    feat: added some search icons to firefox

--- home/browser/firefox.nix
+++ home/browser/firefox.nix
@@ -17,16 +17,18 @@
       search.force = true;
       search.engines = {
         "Nix Packages" = {
+          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = [ "@np" ];
           urls = [{
             template = "https://search.nixos.org/packages";
             params = [
             { name = "query"; value = "{searchTerms}"; }
             ];
           }];
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = [ "@np" ];
         };
         "Nix Options" = {
+          iconUpdateURL = "https://mynixos.com/favicon.ico";
+          updateInterval = 24 * 60 * 60 * 1000;
           definedAliases = [ "@no" ];
           urls = [{
             template = "https://mynixos.com/search";
@@ -36,6 +38,8 @@
           }];
         };
         "NixVim Options" = {
+          iconUpdateURL = "https://github.com/nix-community/nixvim/blob/main/assets/nixvim_logo.svg";
+          updateInterval = 24 * 60 * 60 * 1000;
           definedAliases = [ "@nv" ];
           urls = [{
             template = "https://nix-community.github.io/nixvim";
