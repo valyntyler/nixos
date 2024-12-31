1b62f813163272595b2a0840ecbdd6683d6a77a3
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-31 03:57:34 +0200

    feat: cava color gradient

--- home/cava.nix
+++ home/cava.nix
@@ -1,7 +1,24 @@
+let
+  red = "'#fb4934'";
+  green = "'#b8bb26'";
+  yellow = "'#fabd2f'";
+  blue = "'#83a598'";
+  purple = "'#d3869b'";
+  aqua = "'#8ec07c'";
+in 
 {
   programs.cava.enable = true;
   programs.cava.settings = {
     smoothing.noise_reduction = 15;
     output.channels = "mono";
+    color = {
+      gradient = 1;
+      gradient_color_1 = purple;
+      gradient_color_2 = blue;
+      gradient_color_3 = aqua;
+      gradient_color_4 = green;
+      gradient_color_5 = yellow;
+      gradient_color_6 = red;
+    };
   };
 }
