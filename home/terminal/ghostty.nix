09ac00795579a846a215597ccfcaf69df913d2ca
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-07 22:53:50 +0200

    feat: ghostty config

--- home/terminal/ghostty.nix
+++ home/terminal/ghostty.nix
@@ -1,6 +1,18 @@
 {
   programs.ghostty.enable = true;
   programs.ghostty.settings = {
+    # theme
     theme = "GruvboxDark";
+    # font
+    font-size = 14;
+    font-family = "JetBrainsMono NF";
+    # background
+    background-opacity = 0.9;
+    background-blur = true;
+    # window
+    window-padding-x = 10;
+    window-padding-y = 10;
+    # mouse
+    mouse-hide-while-typing = true;
   };
 }
