56ad8e5c6592b8d9e1e34ac73a7e06b529e98aea
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-10 16:25:25 +0200

    feat: lazygit shell integrations

--- home/lazygit.nix
+++ home/lazygit.nix
@@ -49,4 +49,30 @@
       ];
     };
   };
+
+  programs.zsh.initExtra = ''
+    lg() {
+      export LAZYGIT_NEW_DIR_FILE=~/.lazygit/newdir
+
+      lazygit "$@"
+
+      if [ -f $LAZYGIT_NEW_DIR_FILE ]; then
+        cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
+        rm -f $LAZYGIT_NEW_DIR_FILE > /dev/null
+      fi
+    }
+  '';
+
+  programs.fish.shellInit = ''
+    function lg
+      set -x LAZYGIT_NEW_DIR_FILE ~/.lazygit/newdir
+
+      lazygit $argv
+
+      if test -f $LAZYGIT_NEW_DIR_FILE
+          cd (cat $LAZYGIT_NEW_DIR_FILE)
+          rm -f $LAZYGIT_NEW_DIR_FILE > /dev/null
+      end
+    end
+  '';
 }
