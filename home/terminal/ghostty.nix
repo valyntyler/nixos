4c577b8243a32d9b5040aa0bc30e4a0daddbf792
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-11 00:59:39 +0200

    chore: ghostty file formatting

--- home/terminal/ghostty.nix
+++ home/terminal/ghostty.nix
@@ -1,22 +1,31 @@
 {
-  programs.ghostty.enable = true;
-  programs.ghostty.installBatSyntax = true;
-  programs.ghostty.installVimSyntax = true;
-  programs.ghostty.enableZshIntegration = true;
-  programs.ghostty.enableFishIntegration = true;
-  programs.ghostty.settings = {
-    # theme
-    theme = "GruvboxDark";
-    # font
-    font-size = 14;
-    font-family = "JetBrainsMono NF";
-    # background
-    background-opacity = 0.9;
-    background-blur = true;
-    # window
-    window-padding-x = 10;
-    window-padding-y = 10;
-    # mouse
-    mouse-hide-while-typing = true;
+  programs.ghostty = {
+    enable = true;
+
+    installBatSyntax = true;
+    installVimSyntax = true;
+
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+
+    settings = {
+      # theme
+      theme = "GruvboxDark";
+
+      # font
+      font-size = 14;
+      font-family = "JetBrainsMono NF";
+
+      # background
+      background-opacity = 0.9;
+      background-blur = true;
+
+      # window
+      window-padding-x = 10;
+      window-padding-y = 10;
+
+      # mouse
+      mouse-hide-while-typing = true;
+    };
   };
 }
