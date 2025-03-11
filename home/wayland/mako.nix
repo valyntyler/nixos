03e4668da8561c511b000d74975b7e003e673abe
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-11 15:49:26 +0200

    feat: update mako config

--- home/wayland/mako.nix
+++ home/wayland/mako.nix
@@ -2,10 +2,13 @@
   services.mako = {
     enable = true;
     actions = true;
-    anchor = "bottom-right";
+    anchor = "top-right";
     defaultTimeout = 5000;
     icons = true;
     ignoreTimeout = false;
+    backgroundColor = "#1d2021e6";
+    borderSize = 0;
+    borderRadius = 10;
   };
   # services.mako.extraConfig = "
   #   sort=-time
