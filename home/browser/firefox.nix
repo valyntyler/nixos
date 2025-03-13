a541d3f3826e422a9d286e2655b927dacf86ac98
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-14 01:10:00 +0200

    chore: cleaned firefox module

--- home/browser/firefox.nix
+++ home/browser/firefox.nix
@@ -1,75 +1,27 @@
-{ pkgs, ... }:
-
 {
   # browser
   programs.firefox = {
     enable = true;
     profiles."self" = {
-      id = 0;
       isDefault = false;
-      search.default = "Unduck";
-      search.force = true;
-      search.engines = {
-        "Bing".metaData.hidden = true;
-        "Google".metaData.alias = "@g";
-        "Unduck" = let
-          url = "https://unduck.link";
-        in {
-          urls = [{
-            template = url;
-            params = [{ name = "q"; value = "{searchTerms}"; }];
-            iconUpdateURL = "${url}/search.svg";
-            updateInterval = 24 * 60 * 60 * 1000;
-          }];
-        };
+      search = {
+        default = "Unduck";
+        force = true;
+        engines."Unduck".urls = [{
+          template =  "https://unduck.link";
+          params = [{ name = "q"; value = "{searchTerms}"; }];
+        }];
       };
     };
     profiles."work" = {
-      id = 1;
       isDefault = true;
-      search.default = "Unduck";
-      search.force = true;
-      search.engines = {
-        "Bing".metaData.hidden = true;
-        "Google".metaData.alias = "@g";
-        "Unduck" = let
-          url = "https://unduck.link";
-        in {
-          urls = [{
-            template = url;
-            params = [{ name = "q"; value = "{searchTerms}"; }];
-            iconUpdateURL = "${url}/search.svg";
-            updateInterval = 24 * 60 * 60 * 1000;
-          }];
-        };
-        "Nix Packages" = {
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = [ "@np" ];
-          urls = [{
-            template = "https://search.nixos.org/packages";
-            params = [{ name = "query"; value = "{searchTerms}"; }];
-          }];
-        };
-        "Nix Options" = {
-          iconUpdateURL = "https://mynixos.com/favicon.ico";
-          updateInterval = 24 * 60 * 60 * 1000;
-          definedAliases = [ "@no" ];
-          urls = [{
-            template = "https://mynixos.com/search";
-            params = [
-            { name = "q"; value = "{searchTerms}"; }
-            ];
-          }];
-        };
-        "NixVim Options" = {
-          definedAliases = [ "@nv" ];
-          urls = [{
-            template = "https://nix-community.github.io/nixvim";
-            params = [
-            { name = "search"; value = "{searchTerms}"; }
-            ];
-          }];
-        };
+      search = {
+        default = "Unduck";
+        force = true;
+        engines."Unduck".urls = [{
+          template =  "https://unduck.link";
+          params = [{ name = "q"; value = "{searchTerms}"; }];
+        }];
       };
     };
   };
