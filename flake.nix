9d541fb779d4e7477e01c3db787af2030e120139
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-14 05:03:23 +0200

    chore: reformat flake.nix

--- flake.nix
+++ flake.nix
@@ -6,83 +6,64 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # import home manager flake
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # import custom nixvim flake
-    nixvim = {
-      url = "github:valyntyler/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixvim.url = "github:valyntyler/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
 
     # import spotify overlay flake
-    spotx = {
-      url = "github:oskardotglobal/.dotfiles/nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    spotx.url = "github:oskardotglobal/.dotfiles/nix";
+    spotx.inputs.nixpkgs.follows = "nixpkgs";
 
     # import zen brower flake
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... } @ inputs: {
-    nixosConfigurations = {
-      nixos-desktop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/desktop
-          inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "bak";
+  outputs = { self, nixpkgs, ... } @ inputs:
+  let
+    home-manager-config.home-manager = {
+      users."valyn" = import ./home;
+      extraSpecialArgs = { inherit inputs; };
 
-            # import configuration
-            home-manager.users."valyn" = import ./home;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-          }
-        ];
-      };
-      nixos-laptop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/laptop
-          inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "bak";
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      backupFileExtension = "bak";
+    };
+  in
+  {
+    # desktop
+    nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/desktop
+        home-manager-config
+        inputs.home-manager.nixosModules.home-manager
+      ];
+    };
 
-            # import configuration
-            home-manager.users."valyn" = import ./home;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-          }
-        ];
-      };
-      nixos-iso = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/isoimage
-          inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "bak";
+    # laptop
+    nixosConfigurations.nixos-laptop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/laptop
+        home-manager-config
+        inputs.home-manager.nixosModules.home-manager
+      ];
+    };
 
-            # import configuration
-            home-manager.users."valyn" = import ./home;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-          }
-        ];
-      };
+    # iso image
+    nixosConfigurations.nixos-iso = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/isoimage
+        home-manager-config
+        inputs.home-manager.nixosModules.home-manager
+      ];
     };
   };
 }
-
