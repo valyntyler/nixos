47b731682d1b88073cb2e80202c44346f6abd440
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-14 05:10:12 +0200

    chore: cleaned network.nix

--- hosts/common/core/network.nix
+++ hosts/common/core/network.nix
@@ -10,11 +10,5 @@
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-    # Open ports in the firewall.
-    # firewall.allowedTCPPorts = [ ... ];
-    # firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # firewall.enable = false;
   };
 }
