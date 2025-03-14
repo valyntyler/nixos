3093731a27940ee329fc97aa8535a4636fc9d178
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-14 18:57:37 +0200

    feat: re-enabled some custom firefox search engines

--- home/browser/firefox.nix
+++ home/browser/firefox.nix
@@ -1,6 +1,43 @@
+{ pkgs, ... }:
+
 {
   # browser
-  programs.firefox = {
+  programs.firefox = let
+    unduck.urls = [{
+      template =  "https://unduck.link";
+      params = [{ name = "q"; value = "{searchTerms}"; }];
+    }];
+    nixpackages = {
+      definedAliases = [ "@np" ];
+      icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      urls = [{
+        template = "https://search.nixos.org/packages";
+        params = [
+          { name = "type"; value = "packages"; }
+          { name = "query"; value = "{searchTerms}"; }
+        ];
+      }];
+    };
+    nixoswiki = {
+      "NixOS Wiki" = {
+        definedAliases = [ "@nw" ];
+        iconUpdateURL = "https://wiki.nixos.org/favicon.png";
+        updateInterval = 24 * 60 * 60 * 1000; # every day
+        urls = [{ template = "https://wiki.nixos.org/index.php?search={searchTerms}"; }];
+      };
+    };
+    mynixos = {
+      "MyNixOS" = {
+        definedAliases = [ "@mn" ];
+        iconUpdateURL = "https://wiki.nixos.org/favicon.png";
+        updateInterval = 24 * 60 * 60 * 1000; # every day
+        urls = [{
+          template = "https://mynixos.com/search";
+          params = [{ name = "q"; value = "{searchTerms}"; }];
+        }];
+      };
+    };
+  in {
     enable = true;
     profiles."self" = {
       id = 0;
@@ -8,10 +45,20 @@
       search = {
         default = "Unduck";
         force = true;
-        engines."Unduck".urls = [{
-          template =  "https://unduck.link";
-          params = [{ name = "q"; value = "{searchTerms}"; }];
-        }];
+        engines = {
+          # client-side duckduckgo !bangs
+          "Unduck" = unduck;
+
+          # nixos options
+          "Nix Packages" = nixpackages;
+          "NixOS Wiki" = nixoswiki;
+          "My NixOS" = mynixos;
+
+          # disable defaults
+          "Bing".metaData.hidden = true;
+          "Google".metaData.hidden = true;
+          "Wikipedia".metaData.hidden = true;
+        };
       };
     };
     profiles."work" = {
@@ -20,10 +67,15 @@
       search = {
         default = "Unduck";
         force = true;
-        engines."Unduck".urls = [{
-          template =  "https://unduck.link";
-          params = [{ name = "q"; value = "{searchTerms}"; }];
-        }];
+        engines = {
+          # client-side duckduckgo !bangs
+          "Unduck" = unduck;
+
+          # disable defaults
+          "Bing".metaData.hidden = true;
+          "Google".metaData.hidden = true;
+          "Wikipedia".metaData.hidden = true;
+        };
       };
     };
   };
