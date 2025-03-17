8933829c7a77d859fa26889e236054ff3e7e405a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-17 06:34:27 +0200

    chore: cleaned desktop module

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,5 +1,3 @@
-{ pkgs, ... }:
-
 {
   imports = [
     ./hardware-configuration.nix
@@ -9,7 +7,8 @@
     ../common/optional/programs/steam.nix
   ];
 
-  networking.hostName = "nixos-desktop"; # Define your hostname.
+  # Set hostname
+  networking.hostName = "nixos-desktop";
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -17,31 +16,6 @@
   # Enable the Flakes feature and the accompanying new nix command-line tool
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Enable shell(s)
-  programs = {
-    fish.enable = true;
-    zsh.enable = true;
-  };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-
+  # Initial NixOS version
+  system.stateVersion = "24.11";
 }
