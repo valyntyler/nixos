defeed0cb8b9f22148514465360598afd7728b9d
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-18 16:13:14 +0200

    fix: remove broken anyrun declarations

--- home/anyrun.nix
+++ home/anyrun.nix
@@ -21,24 +21,7 @@
 
       plugins = [
         inputs.anyrun.packages.${pkgs.system}.applications
-        inputs.anyrun.packages.${pkgs.system}.anyrun-with-all-plugins
       ];
     };
-
-    # Inline comments are supported for language injection into
-    # multi-line strings with Treesitter! (Depends on your editor)
-    extraCss = /*css */ ''
-      .some_class {
-        background: red;
-      }
-    '';
-
-    extraConfigFiles."some-plugin.ron".text = ''
-      Config(
-        // for any other plugin
-        // this file will be put in ~/.config/anyrun/some-plugin.ron
-        // refer to docs of xdg.configFile for available options
-      )
-    '';
   };
 }
