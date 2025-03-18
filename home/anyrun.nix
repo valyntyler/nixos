2fce7b8d72cd0db46b906d687fe211af7d73577f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-18 16:42:28 +0200

    feat: anyrun plugins

--- home/anyrun.nix
+++ home/anyrun.nix
@@ -21,6 +21,15 @@
 
       plugins = [
         inputs.anyrun.packages.${pkgs.system}.applications
+        inputs.anyrun.packages.${pkgs.system}.dictionary
+        inputs.anyrun.packages.${pkgs.system}.kidex
+        inputs.anyrun.packages.${pkgs.system}.randr
+        inputs.anyrun.packages.${pkgs.system}.rink
+        inputs.anyrun.packages.${pkgs.system}.shell
+        inputs.anyrun.packages.${pkgs.system}.stdin
+        inputs.anyrun.packages.${pkgs.system}.symbols
+        inputs.anyrun.packages.${pkgs.system}.translate
+        inputs.anyrun.packages.${pkgs.system}.websearch
       ];
     };
   };
