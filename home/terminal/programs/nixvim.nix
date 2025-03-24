7e822d9bbd71f1a0aa5480af164b36461d21e5cd
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-24 22:30:05 +0200

    feat: nixvim default editor

--- home/terminal/programs/nixvim.nix
+++ home/terminal/programs/nixvim.nix
@@ -3,4 +3,8 @@
 {
   # enable nixvim
   home.packages = [ inputs.nixvim.packages.${pkgs.system}.default ];
+
+  # set as default editor
+  programs.nushell.environmentVariables.EDITOR = "nvim";
+  programs.nushell.environmentVariables.VISUAL = "nvim";
 }
