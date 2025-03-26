0eb1cf7f20a7c9f7dd65709b78e6a7f23170f5a4
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-26 16:01:44 +0200

    feat: enable git lfs

--- home/terminal/programs/git.nix
+++ home/terminal/programs/git.nix
@@ -4,6 +4,7 @@
     userName = "Valyn Tyler";
     userEmail = "115489989+ValynTyler@users.noreply.github.com";
     extraConfig.init.defaultBranch = "main";
+    lfs.enable = true;
   };
 
   programs.gh = {
