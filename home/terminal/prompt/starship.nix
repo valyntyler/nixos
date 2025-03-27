13964f945e04001e2635888120474745c13cc60a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-27 14:39:42 +0200

    feat: enable starship transience for nu

--- home/terminal/prompt/starship.nix
+++ home/terminal/prompt/starship.nix
@@ -56,4 +56,14 @@
       style = "bold bright-blue";
     }; 
   };
+
+  # enable nushell transience
+  programs.nushell.extraConfig = ""
+    + "\n" + ''$env.TRANSIENT_PROMPT_COMMAND = ^starship module character''
+    + "\n" + ''$env.TRANSIENT_PROMPT_INDICATOR = ""''
+    + "\n" + ''$env.TRANSIENT_PROMPT_INDICATOR_VI_INSERT = ""''
+    + "\n" + ''$env.TRANSIENT_PROMPT_INDICATOR_VI_NORMAL = ""''
+    + "\n" + ''$env.TRANSIENT_PROMPT_MULTILINE_INDICATOR = ""''
+    + "\n" + ''$env.TRANSIENT_PROMPT_COMMAND_RIGHT = ^starship module time''
+    ;
 }
