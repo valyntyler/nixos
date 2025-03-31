14ee24602803372d1d075e2aca1e7d893f315edc
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-31 14:43:33 +0300

    chore: starship env fmt

--- home/terminal/prompt/starship.nix
+++ home/terminal/prompt/starship.nix
@@ -65,5 +65,5 @@
     + "\n" + ''$env.TRANSIENT_PROMPT_INDICATOR_VI_NORMAL = ""''
     + "\n" + ''$env.TRANSIENT_PROMPT_MULTILINE_INDICATOR = ""''
     + "\n" + ''$env.TRANSIENT_PROMPT_COMMAND_RIGHT = ^starship module time''
-    ;
+    + "\n";
 }
