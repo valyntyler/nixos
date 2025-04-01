81384913272e6371ac76675dad251a25f19abd3e
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-01 19:46:39 +0300

    feat: enable nushell completions

--- home/terminal/shell/nushell.nix
+++ home/terminal/shell/nushell.nix
@@ -6,6 +6,10 @@
     plugins = [ pkgs.nushellPlugins.formats ];
     settings = {
       show_banner = false;
+      completions.external = {
+        enable = true;
+        max_results = 200;
+      };
     };
     extraConfig = "${pkgs.nitch}/bin/nitch";
   };
