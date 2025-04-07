4e4b50bdf2731b35d0dac052b3c331ec3aede71f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-07 18:14:58 +0300

    feat: vscode extensions

--- home/software/editor/vscodium.nix
+++ home/software/editor/vscodium.nix
@@ -3,4 +3,9 @@
 {
   programs.vscode.enable = true;
   programs.vscode.package = pkgs.vscodium;
+  programs.vscode.profiles.default = {
+    extensions = with pkgs.vscode-extensions; [
+      vscodevim.vim
+    ];
+  };
 }
