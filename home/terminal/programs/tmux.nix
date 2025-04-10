2c1b127ed66863a4840a61575fd8e689217d47ff
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-10 20:14:12 +0300

    fix: tmux package

--- home/terminal/programs/tmux.nix
+++ home/terminal/programs/tmux.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 
 {
-  programs.tmux.enable = true;
+  home.packages = with pkgs; [ tmux ];
+  # programs.tmux.enable = true;
   # programs.tmux.plugins = with pkgs.tmuxPlugins; [
   #   tmux-floax
   #   vim-tmux-navigator
