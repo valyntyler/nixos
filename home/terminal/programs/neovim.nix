9debd968b3636e56264c39974d09081404e43468
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-13 18:55:25 +0300

    feat: neovim plugins

--- home/terminal/programs/neovim.nix
+++ home/terminal/programs/neovim.nix
@@ -1,4 +1,10 @@
+{ pkgs, ... }:
+
 {
   programs.neovim.enable = true;
   programs.neovim.defaultEditor = true;
+  programs.neovim.plugins = with pkgs.vimPlugins; [
+    gruvbox
+    vim-nix
+  ];
 }
