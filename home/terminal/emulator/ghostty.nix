5aa4d74de72f690df078a3b746ea58bac87be05a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-13 19:01:02 +0300

    chore: remove ghostty config

--- home/terminal/emulator/ghostty.nix
+++ home/terminal/emulator/ghostty.nix
@@ -5,24 +5,5 @@
     # shell integrations
     enableZshIntegration = true;
     enableFishIntegration = true;
-
-    # disable default binds
-    clearDefaultKeybinds = true;
-
-    settings = {
-      # theme
-      theme = "GruvboxDark";
-
-      # font
-      font-size = 14;
-      font-family = "JetBrainsMono NF";
-
-      # window
-      window-padding-x = 10;
-      window-padding-y = 10;
-
-      # mouse
-      mouse-hide-while-typing = true;
-    };
   };
 }
