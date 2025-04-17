d52eadc56d7c68d58ab8e54d386f9a547345cdd7
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-17 16:22:42 +0300

    chore: update tmux.nix

--- home/terminal/programs/tmux.nix
+++ home/terminal/programs/tmux.nix
@@ -1,10 +1,9 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ tmux ];
-  # programs.tmux.enable = true;
-  # programs.tmux.plugins = with pkgs.tmuxPlugins; [
-  #   tmux-floax
-  #   vim-tmux-navigator
-  # ];
+  home.packages = with pkgs; [
+    tmux
+    tmuxPlugins.tmux-floax
+    tmuxPlugins.vim-tmux-navigator
+  ];
 }
