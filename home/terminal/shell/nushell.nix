d8e9b13a0ba4d9a08107b2ecd33758c1830e8d90
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-17 16:05:01 +0300

    feat: update nushell edit mode

--- home/terminal/shell/nushell.nix
+++ home/terminal/shell/nushell.nix
@@ -5,6 +5,7 @@
     enable = true;
     plugins = [ pkgs.nushellPlugins.formats ];
     settings = {
+      edit_mode = "vi";
       show_banner = false;
       completions.external = {
         enable = true;
