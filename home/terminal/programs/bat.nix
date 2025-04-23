d09553ee9b2f2c0ce5374e19fdb9bdf76ffa72ab
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-24 00:04:13 +0300

    feat: update bat.nix

--- home/terminal/programs/bat.nix
+++ home/terminal/programs/bat.nix
@@ -1,3 +1,11 @@
 {
   programs.bat.enable = true;
+  programs.bat.config = {
+    theme = "gruvbox-dark";
+    paging = "never";
+    map-syntax = [
+      "*.ino:C++"
+      ".ignore:Git Ignore"
+    ];
+  };
 }
