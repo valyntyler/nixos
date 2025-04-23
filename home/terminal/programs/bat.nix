bb97b7506f2b53090b43d1ad6750d0fe45215cde
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-24 00:10:40 +0300

    feat: enable bat extraPackages

--- home/terminal/programs/bat.nix
+++ home/terminal/programs/bat.nix
@@ -1,11 +1,23 @@
+{ pkgs, ... }:
+
 {
-  programs.bat.enable = true;
-  programs.bat.config = {
-    theme = "gruvbox-dark";
-    paging = "never";
-    map-syntax = [
-      "*.ino:C++"
-      ".ignore:Git Ignore"
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "gruvbox-dark";
+      paging = "never";
+      map-syntax = [
+        "*.ino:C++"
+        ".ignore:Git Ignore"
+      ];
+    };
+    extraPackages = with pkgs.bat-extras; [
+      batgrep 
+      batman 
+      batpipe
+      batwatch
+      batdiff
+      prettybat
     ];
   };
 }
