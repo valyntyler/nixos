ad720474dfc53079623095d296ae54d9663ccbd1
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-08 21:11:31 +0300

    feat: hunspell packge and dicts

--- home/software/packages.nix
+++ home/software/packages.nix
@@ -4,6 +4,9 @@
   home.packages = with pkgs; [
     appimage-run
     galaxy-buds-client
+    hunspell
+    hunspellDicts.en_US
+    hunspellDicts.ro_RO
     libreoffice
     prismlauncher
     tigervnc
