62c6f269e074d4c3d0ae1ab524af24bbe8046ceb
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-18 23:31:53 +0300

    feat: xdg mime types

--- host/common/core/xdg.nix
+++ host/common/core/xdg.nix
@@ -1,7 +1,20 @@
 {
-  xdg.portal = {
-    enable = true;
-    xdgOpenUsePortal = true;
-    wlr.enable = true;
+  xdg = {
+    portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      wlr.enable = true;
+    };
+    mime = {
+      enable = true;
+      defaultApplications = {
+        "application/pdf" = "evince.desktop";
+        "image/png" = "org.gnome.Loupe.desktop";
+        "image/jpeg" = "org.gnome.Loupe.desktop";
+        "application/zip" = "org.gnome.FileRoller.desktop";
+        "image/svg+xml" = "org.gnome.Loupe.desktop";
+        "application/json" = "org.gnome.TextEditor.desktop";
+      };
+    };
   };
 }
