8c1dbd88489f3c58a5813fd3e5552b12219e864f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-21 21:52:50 +0300

    chore: update nix.nix

--- host/common/core/nix.nix
+++ host/common/core/nix.nix
@@ -1,4 +1,10 @@
 {
-  # Enable flakes and nix command
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings = {
+    # Enable flakes and nix command
+    experimental-features = ["nix-command" "flakes"];
+
+    # Add binary cache
+    trusted-substituters = ["https://nix-community.cachix.org"];
+    trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+  };
 }
