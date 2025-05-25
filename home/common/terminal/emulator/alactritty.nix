96ec91c60ab7ca436fdd29a50535bda4b02e5122
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-26 00:42:08 +0300

    chore: update alacritty.nix

--- home/common/terminal/emulator/alactritty.nix
+++ home/common/terminal/emulator/alactritty.nix
@@ -1,15 +1,11 @@
 {
   programs.alacritty.enable = true;
   programs.alacritty.settings = {
-    # window
     window = {
       padding.x = 10;
       padding.y = 10;
     };
-
     env.term = "xterm-256color";
-
-    # mouse
     mouse.hide_when_typing = true;
   };
 }
