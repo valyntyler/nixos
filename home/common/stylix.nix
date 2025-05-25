d1034a66c22ed4955ff713f5d6fb2043139fb924
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-26 00:33:11 +0300

    feat: enable stylix fonts

--- home/common/stylix.nix
+++ home/common/stylix.nix
@@ -10,5 +10,24 @@
     polarity = "dark";
     image = ../../assets/rocca-pietore.jpg;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
+
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono NF";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
   };
 }
