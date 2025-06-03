cfd6930f49dbc7ae92c78cefd23774adf3a0931a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-03 18:52:20 +0300

    fix: laptop graphics hardware

--- host/laptop/default.nix
+++ host/laptop/default.nix
@@ -6,6 +6,8 @@
     ../common/optional/programs/spotify.nix
     ../common/optional/programs/steam.nix
     ../common/optional/services/kanata.nix
+    ../common/optional/hardware/graphics.nix
+    ../common/optional/hardware/nvidia.nix
   ];
 
   # Set hostname
