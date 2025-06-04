6dde9622119bf99e6a7d1ac065d7cfb30ce70752
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-04 04:03:53 +0300

    feat: update creation.nix

--- home/common/software/creation.nix
+++ home/common/software/creation.nix
@@ -1,10 +1,13 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
+    audacity
     blender
     freecad
     gimp3
     inkscape
     krita
+    muse-sounds-manager
+    musescore
 
     # unfree
     davinci-resolve
