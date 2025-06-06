fb17904fa37a9cc795a95b78381a0430467a3f62
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-07 01:54:13 +0300

    feat: update open-rgb.nix

--- host/common/optional/hardware/open-rgb.nix
+++ host/common/optional/hardware/open-rgb.nix
@@ -1,6 +1,8 @@
 {pkgs, ...}: {
-  services.hardware.openrgb.enable = true;
-  environment.systemPackages = [
-    pkgs.openrgb
-  ];
+  services.hardware.openrgb = {
+    enable = true;
+    package = pkgs.openrgb-with-all-plugins;
+    motherboard = "amd";
+  };
+  environment.systemPackages = with pkgs; [openrgb-with-all-plugins];
 }
