2a0461037c80ed42f1c7ef83db9fc4b8b5f95371
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-07 00:00:00 +0300

    refactor: rename xbox.nix

--- host/desktop/default.nix
+++ host/desktop/default.nix
@@ -7,7 +7,7 @@
     ../common/optional/programs/steam.nix
     ../common/optional/hardware/graphics.nix
     ../common/optional/hardware/nvidia.nix
-    ../common/optional/hardware/xone.nix
+    ../common/optional/hardware/xbox.nix
   ];
 
   # Set hostname
