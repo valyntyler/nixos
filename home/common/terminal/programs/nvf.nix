2fb024275983c7d70f20f988c64a1117622f9265
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-08 23:10:47 +0300

    temp: gdtoolkit package

--- home/common/terminal/programs/nvf.nix
+++ home/common/terminal/programs/nvf.nix
@@ -1,5 +1,12 @@
-{inputs, ...}: {
-  home.packages = [inputs.nvf.packages.x86_64-linux.default];
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  home.packages = [
+    inputs.nvf.packages.x86_64-linux.default
+    pkgs.gdtoolkit_4
+  ];
 
   programs.nushell.environmentVariables = {
     EDITOR = "nvim";
