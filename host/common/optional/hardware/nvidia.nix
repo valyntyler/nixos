50fd3247c5e31ac47b35af4ef4e2a028c3dc7bd9
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-13 00:10:07 +0300

    feat: update nvidia.nix

--- host/common/optional/hardware/nvidia.nix
+++ host/common/optional/hardware/nvidia.nix
@@ -1,10 +1,18 @@
 {
   # Enable GPU drivers
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [
+    "modesetting"
+    "nvidia"
+  ];
   hardware.nvidia = {
-    open = false; # open source drivers
+    open = true;
     modesetting.enable = true;
     powerManagement.enable = true;
+    prime = {
+      offload.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
   environment.variables = {
     "PRIME_RENDER_OFFLOAD" = "1";
