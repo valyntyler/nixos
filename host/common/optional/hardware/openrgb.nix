5191ef8645ff35ed7fc99febb3bc6faece59a6d0
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-24 17:46:50 +0300

    fix: disable openrgb service

--- host/common/optional/hardware/openrgb.nix
+++ host/common/optional/hardware/openrgb.nix
@@ -2,10 +2,5 @@
   services.udev.packages = [pkgs.openrgb];
   boot.kernelModules = ["i2c-dev"];
   hardware.i2c.enable = true;
-  services.hardware.openrgb = {
-    enable = true;
-    package = pkgs.openrgb-with-all-plugins;
-    motherboard = "amd";
-  };
   environment.systemPackages = with pkgs; [openrgb-with-all-plugins];
 }
