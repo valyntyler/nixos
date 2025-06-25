9b61a5525d0b572093648e9227ca9db5425ed247
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-25 15:13:52 +0300

    fix: enable openrgb service

--- host/common/optional/hardware/openrgb.nix
+++ host/common/optional/hardware/openrgb.nix
@@ -1,6 +1,4 @@
 {pkgs, ...}: {
-  services.udev.packages = [pkgs.openrgb];
-  boot.kernelModules = ["i2c-dev"];
-  hardware.i2c.enable = true;
   environment.systemPackages = with pkgs; [openrgb-with-all-plugins];
+  services.hardware.openrgb.enable = true;
 }
