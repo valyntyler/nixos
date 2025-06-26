ff6dd1a7b10beca351aba38da83a8b0d9bbc6e6f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-26 15:58:48 +0300

    fix: remove nushell highlight plugin

--- home/common/terminal/shell/nushell.nix
+++ home/common/terminal/shell/nushell.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   programs.nushell = {
     enable = true;
-    plugins = with pkgs.nushellPlugins; [formats highlight];
+    plugins = with pkgs.nushellPlugins; [formats];
     settings = {
       show_banner = false;
       completions.external = {
