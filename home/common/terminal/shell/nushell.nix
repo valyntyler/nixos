2f75649281115855cee7d2244507611e1ea47ef7
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-26 15:57:18 +0300

    fix: remove nushell net plugin

--- home/common/terminal/shell/nushell.nix
+++ home/common/terminal/shell/nushell.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   programs.nushell = {
     enable = true;
-    plugins = with pkgs.nushellPlugins; [formats net highlight];
+    plugins = with pkgs.nushellPlugins; [formats highlight];
     settings = {
       show_banner = false;
       completions.external = {
