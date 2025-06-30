3aa801c5fd4e735c34991decb6664e12929182b2
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-30 16:03:47 +0300

    fix: enable thinkcentre ssh root access

--- host/centre0/default.nix
+++ host/centre0/default.nix
@@ -11,6 +11,12 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
+  # Allow root access
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0bwDvMZZPs2ASyR+3l1tMqj3LQ6AmFhtEN9SVfL/EY valyn@nixos-desktop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEPPZCKMfsY3wdpHIfYPQ2mGTwaMOp5ANKjwG4hs3Iig valyn@nixos-laptop"
+  ];
+
   # Initial NixOS version
   system.stateVersion = "24.11";
 }
