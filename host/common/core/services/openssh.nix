a98fd09afa0bd57000d496cf57a93c43f172c3f6
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-02 17:35:37 +0300

    feat: enable root access with ssh

--- host/common/core/services/openssh.nix
+++ host/common/core/services/openssh.nix
@@ -10,4 +10,8 @@
       PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
     };
   };
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0bwDvMZZPs2ASyR+3l1tMqj3LQ6AmFhtEN9SVfL/EY valyn@nixos-desktop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEPPZCKMfsY3wdpHIfYPQ2mGTwaMOp5ANKjwG4hs3Iig valyn@nixos-laptop"
+  ];
 }
