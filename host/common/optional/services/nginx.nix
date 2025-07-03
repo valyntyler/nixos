917d35906855f068970a30f7bba97eccfca4d8bc
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-03 11:59:23 +0300

    refactor: update acme config

--- host/common/optional/services/nginx.nix
+++ host/common/optional/services/nginx.nix
@@ -17,10 +17,7 @@
   };
   security.acme = {
     acceptTerms = true;
-    certs = {
-      "danban.ro".email = "certify@danban.ro";
-      "valyntyler.com".email = "certify@valyntyler.com";
-    };
+    defaults.email = "certify@valyntyler.com";
   };
   networking.firewall.allowedTCPPorts = [80 443];
 }
