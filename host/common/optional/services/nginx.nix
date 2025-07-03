dc8665836dbcc8161c387fe27915678fa3736fed
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-03 11:50:06 +0300

    feat: enable my website through nginx

--- host/common/optional/services/nginx.nix
+++ host/common/optional/services/nginx.nix
@@ -6,11 +6,17 @@
       forceSSL = true;
       root = "/var/www/danban.ro";
     };
+    virtualHosts."valyntyler.com" = {
+      enableACME = true;
+      forceSSL = true;
+      root = "/var/www/valyntyler.com";
+    };
   };
   security.acme = {
     acceptTerms = true;
     certs = {
       "danban.ro".email = "certify@danban.ro";
+      "valyntyler.com".email = "certify@valyntyler.com";
     };
   };
   networking.firewall.allowedTCPPorts = [80 443];
