01aecfc2c889725705218e85aa298fbad8fe6cb3
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-11 19:11:57 +0300

    feat: full clear nushell keybinding

--- home/common/terminal/shell/nushell.nix
+++ home/common/terminal/shell/nushell.nix
@@ -8,6 +8,22 @@
         enable = true;
         max_results = 200;
       };
+      keybindings = [
+        {
+          name = "completion_menu";
+          modifier = "control";
+          keycode = "char_l";
+          mode = "emacs";
+          event = [
+            {edit = "Clear";}
+            {
+              edit = "InsertString";
+              value = "clear";
+            }
+            {send = "Enter";}
+          ];
+        }
+      ];
     };
     shellAliases = {
       e = "nvim";
