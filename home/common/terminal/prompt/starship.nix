fafed5a92cb3e007ddd962addf17441cf19df45a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-15 18:56:49 +0300

    fix: starship transient right-prompt

--- home/common/terminal/prompt/starship.nix
+++ home/common/terminal/prompt/starship.nix
@@ -3,19 +3,8 @@
   programs.starship.enableTransience = true;
 
   # enable nushell transience
-  programs.nushell.extraConfig =
-    ""
-    + "\n"
-    + ''$env.TRANSIENT_PROMPT_COMMAND = ^starship module character''
-    + "\n"
-    + ''$env.TRANSIENT_PROMPT_INDICATOR = ""''
-    + "\n"
-    + ''$env.TRANSIENT_PROMPT_INDICATOR_VI_INSERT = ""''
-    + "\n"
-    + ''$env.TRANSIENT_PROMPT_INDICATOR_VI_NORMAL = ""''
-    + "\n"
-    + ''$env.TRANSIENT_PROMPT_MULTILINE_INDICATOR = ""''
-    + "\n"
-    + ''$env.TRANSIENT_PROMPT_COMMAND_RIGHT = ^starship module time''
-    + "\n";
+  programs.nushell.extraConfig = ''
+    $env.TRANSIENT_PROMPT_COMMAND = {|| starship module character }
+    $env.TRANSIENT_PROMPT_COMMAND_RIGHT = {|| starship module time }
+  '';
 }
