f1bf127bc7df81ea249d24540b807f893b014e52
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-20 20:36:00 +0300

    feat: enable mullvad

--- host/laptop/default.nix
+++ host/laptop/default.nix
@@ -7,6 +7,7 @@
     ../common/optional/programs/spotify.nix
     ../common/optional/programs/steam.nix
     ../common/optional/services/kanata.nix
+    ../common/optional/services/mullvad.nix
     ../common/optional/hardware/bluetooth.nix
     ../common/optional/hardware/graphics.nix
     ../common/optional/hardware/nvidia.nix
