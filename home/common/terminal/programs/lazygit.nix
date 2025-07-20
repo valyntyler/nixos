9a719a0be20d0d4c6e9626372375631e95c27ed6
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-20 18:24:32 +0300

    fix: disable lazygit init repo prompt

--- home/common/terminal/programs/lazygit.nix
+++ home/common/terminal/programs/lazygit.nix
@@ -1,23 +1,28 @@
 {
-  programs.lazygit.enable = true;
-  programs.lazygit.settings.gui = {
-    # scrolloff
-    scrollPastBottom = true;
-    scrollOffMargin = 999;
-    scrollOffBehaviour = "margin";
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      notARepository = "quit";
+      gui = {
+        # scrolloff
+        scrollPastBottom = true;
+        scrollOffMargin = 999;
+        scrollOffBehaviour = "margin";
 
-    # spinner
-    spinner.rate = 60;
-    spinner.frames = [
-      # (find more at https://www.npmjs.com/package/cli-spinners)
-      "←"
-      "↖"
-      "↑"
-      "↗"
-      "→"
-      "↘"
-      "↓"
-      "↙"
-    ];
+        # spinner
+        spinner.rate = 60;
+        spinner.frames = [
+          # (find more at https://www.npmjs.com/package/cli-spinners)
+          "←"
+          "↖"
+          "↑"
+          "↗"
+          "→"
+          "↘"
+          "↓"
+          "↙"
+        ];
+      };
+    };
   };
 }
