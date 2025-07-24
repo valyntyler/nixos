20b854e67a810bd24a8fcfcec11027b24e3f7ec7
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-25 01:12:34 +0300

    fix: update nvidia.nix

--- host/common/optional/hardware/nvidia.nix
+++ host/common/optional/hardware/nvidia.nix
@@ -1,5 +1,4 @@
 {
-  # Enable GPU drivers
   services.xserver.videoDrivers = [
     "modesetting"
     "nvidia"
@@ -7,15 +6,20 @@
   hardware.nvidia = {
     open = true;
     modesetting.enable = true;
-    powerManagement.enable = true;
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
     prime = {
-      offload.enable = true;
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
+      reverseSync = {
+        enable = true;
+      };
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
     };
-  };
-  environment.variables = {
-    "PRIME_RENDER_OFFLOAD" = "1";
-    "GLX_VENDOR_LIBRARY_NAME" = "nvidia";
   };
 }
