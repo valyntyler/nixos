87602da0c65d795f817453317022f7810e9aace0
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-08-12 17:54:27 +0300

    fix: remove spotx overlay

--- host/common/core/nixpkgs.nix
+++ host/common/core/nixpkgs.nix
@@ -2,7 +2,6 @@
   nixpkgs = {
     config.allowUnfree = true;
     overlays = [
-      (import ../../../overlay/spotx.nix)
       (import ../../../overlay/yazi-flavors/gruvbox-dark.nix)
     ];
   };
