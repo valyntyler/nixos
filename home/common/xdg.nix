05faee9fdd3d73c97b68951ae8e7dd3c704037f5
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-09-15 14:22:49 +0200

    refactor: update xdg.nix

--- home/common/xdg.nix
+++ home/common/xdg.nix
@@ -1,34 +1,48 @@
 {
-  xdg.enable = true;
+  xdg = {
+    enable = true;
 
-  # enable autostart
-  xdg.autostart.enable = true;
+    # enable autostart
+    autostart.enable = true;
 
-  # hide some desktop entries
-  xdg.desktopEntries = {
-    btop = {
-      name = "";
-      noDisplay = true;
+    # hide some desktop entries
+    desktopEntries = {
+      btop = {
+        name = "";
+        noDisplay = true;
+      };
+      fish = {
+        name = "";
+        noDisplay = true;
+      };
+      nvim = {
+        name = "";
+        noDisplay = true;
+      };
+      yazi = {
+        name = "";
+        noDisplay = true;
+      };
+      spotify-tray = {
+        name = "";
+        noDisplay = true;
+      };
+      nvtop = {
+        name = "";
+        noDisplay = true;
+      };
     };
-    fish = {
-      name = "";
-      noDisplay = true;
-    };
-    nvim = {
-      name = "";
-      noDisplay = true;
-    };
-    yazi = {
-      name = "";
-      noDisplay = true;
-    };
-    spotify-tray = {
-      name = "";
-      noDisplay = true;
-    };
-    nvtop = {
-      name = "";
-      noDisplay = true;
+
+    # set default apps
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/*" = ["org.gnome.TextEditor.desktop"];
+        "image/*" = ["org.gnome.Loupe.desktop"];
+        "application/pdf" = ["org.gnome.Papers.desktop"];
+        "application/zip" = ["org.gnome.FileRoller.desktop"];
+        "application/json" = ["org.gnome.TextEditor.desktop"];
+      };
     };
   };
 }
--- host/common/optional/xdg.nix
+++ host/common/optional/xdg.nix
@@ -5,15 +5,5 @@
       xdgOpenUsePortal = true;
       wlr.enable = true;
     };
-    mime = {
-      enable = true;
-      defaultApplications = {
-        "text/*" = ["org.gnome.TextEditor.desktop"];
-        "image/*" = ["org.gnome.Loupe.desktop"];
-        "application/pdf" = ["org.gnome.Papers.desktop"];
-        "application/zip" = ["org.gnome.FileRoller.desktop"];
-        "application/json" = ["org.gnome.TextEditor.desktop"];
-      };
-    };
   };
 }
