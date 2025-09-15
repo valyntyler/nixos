83705691b393697909fc152eccf1db68a6ac024a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-09-15 13:30:20 +0200

    refactor: create software/launcher/

---
+++ home/common/software/launcher/default.nix
@@ -0,0 +1,6 @@
+{
+  imports = [
+    ./sherlock.nix
+    ./wofi.nix
+  ];
+}
--- home/valyn/default.nix
+++ home/valyn/default.nix
@@ -3,10 +3,9 @@
     ../common/software/browser
     ../common/software/editor
     ../common/software/email
+    ../common/software/launcher
     ../common/software/creation.nix
     ../common/software/packages.nix
-    ../common/software/sherlock.nix
-    ../common/software/wofi.nix
     ../common/terminal/emulator
     ../common/terminal/programs
     ../common/terminal/prompt
