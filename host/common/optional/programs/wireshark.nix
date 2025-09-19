1eea73a373d400b93e9dac5971179cbdf1d20bf4
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-09-19 12:12:41 +0200

    feat: update wireshark.nix

--- host/common/optional/programs/wireshark.nix
+++ host/common/optional/programs/wireshark.nix
@@ -1,5 +1,9 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [wireshark];
+  users.users."valyn".extraGroups = ["wireshark"];
+  environment.systemPackages = with pkgs; [
+    termshark
+    wireshark
+  ];
   programs.wireshark = {
     enable = true;
     dumpcap.enable = true;
