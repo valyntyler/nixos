4a1e895a3d91f02b3688a4d6b0b9b79f714e2075
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-07 01:08:56 +0200

    feat: update stick imports

--- host/stick/default.nix
+++ host/stick/default.nix
@@ -1,6 +1,9 @@
 {modulesPath, ...}: {
   imports = [
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    # ../common/core
+    ../common/optional/gnome.nix
+    ../common/optional/services/kanata.nix
   ];
 
   nixpkgs.hostPlatform = "x86_64-linux";
