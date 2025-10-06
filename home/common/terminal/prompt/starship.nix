8c727679e7360a991d9d5447cf9da9982f2c996f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-06 22:34:01 +0200

    refactor: declarative starship dotfiles

--- home/common/terminal/prompt/starship.nix
+++ home/common/terminal/prompt/starship.nix
@@ -1,6 +1,9 @@
 {
-  programs.starship.enable = true;
-  programs.starship.enableTransience = true;
+  programs.starship = {
+    enable = true;
+    enableTransience = true;
+    settings = builtins.fromTOML (builtins.readFile ./starship.toml);
+  };
 
   # enable nushell transience
   programs.nushell.extraConfig = ''
