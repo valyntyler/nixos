e2970c258a92712cbec7c1e9e1ef9270d788d1b7
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-07 01:08:45 +0200

    feat: create home thumb

---
+++ home/thumb/default.nix
@@ -0,0 +1,31 @@
+{inputs, ...}: {
+  imports = [
+    # ../common/software/browser
+    # ../common/software/editor
+    # ../common/software/email
+    # ../common/software/launcher
+    # ../common/software/creation.nix
+    # ../common/software/packages.nix
+    # ../common/terminal/emulator
+    # ../common/terminal/programs
+    # ../common/terminal/prompt
+    # ../common/terminal/services
+    # ../common/terminal/shell
+    # ../common/terminal/packages.nix
+    # ../common/dconf.nix
+    # ../common/fonts.nix
+    # ../common/gtk.nix
+    # ../common/xdg.nix
+
+    # modules
+    ../../modules/home-manager
+    inputs.zen-browser.homeModules.beta
+  ];
+
+  home.username = "thumb";
+  home.homeDirectory = "/home/thumb";
+
+  home.stateVersion = "24.11";
+
+  programs.home-manager.enable = true;
+}
