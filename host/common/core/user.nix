d9c31912313e468b7b9d97722e20e9fd23bd951a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-07 00:15:48 +0200

    chore: update user.nix

--- host/common/core/user.nix
+++ host/common/core/user.nix
@@ -10,9 +10,5 @@
     description = lib.toSentenceCase user;
     extraGroups = ["networkmanager" "wheel" "dialout" "uucp"];
     shell = pkgs.nushell;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0bwDvMZZPs2ASyR+3l1tMqj3LQ6AmFhtEN9SVfL/EY valyn@nixos-desktop"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEPPZCKMfsY3wdpHIfYPQ2mGTwaMOp5ANKjwG4hs3Iig valyn@nixos-laptop"
-    ];
   };
 }
