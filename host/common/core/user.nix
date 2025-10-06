4f1c7a758e8a1727c587fbd7189da59b23997c52
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-07 00:22:12 +0200

    fmt: user.nix

--- host/common/core/user.nix
+++ host/common/core/user.nix
@@ -4,11 +4,15 @@
   user,
   ...
 }: {
-  # Add a user
   users.users.${user} = {
     isNormalUser = true;
-    description = lib.toSentenceCase user;
-    extraGroups = ["networkmanager" "wheel" "dialout" "uucp"];
     shell = pkgs.nushell;
+    description = lib.toSentenceCase user;
+    extraGroups = [
+      "dialout"
+      "networkmanager"
+      "uucp"
+      "wheel"
+    ];
   };
 }
