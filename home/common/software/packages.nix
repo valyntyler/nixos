88944de69e6f0bdb6ce68d4e5e6ae2446c9a963a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-07 14:05:23 +0200

    chore: update packages.nix

--- home/common/software/packages.nix
+++ home/common/software/packages.nix
@@ -3,16 +3,11 @@
     appimage-run
     bitwarden-desktop
     galaxy-buds-client
-    gnome-power-manager
-    hunspell
-    hunspellDicts.en_US
-    hunspellDicts.ro_RO
     libreoffice-fresh
     mullvad-vpn
     obsidian
     prismlauncher
     pwvucontrol
-    rpi-imager
     signal-desktop
     tigervnc
     vlc
