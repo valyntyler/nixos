5578acfb7ca733e2f95d9e688b0f92b1fbaed812
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-13 19:28:39 +0200

    fix: update nh.nix

--- home/common/terminal/programs/nh.nix
+++ home/common/terminal/programs/nh.nix
@@ -1,9 +1,12 @@
-{
+let
+  flake = /home/valyn/.nixos;
+in {
   programs.nh = {
+    inherit flake;
     enable = true;
-    flake = /home/valyn/.nixos;
-    clean = {
-      enable = true;
-    };
+    clean.enable = true;
   };
+
+  # zellij integration
+  programs.nushell.environmentVariables.NH_FLAKE = "/home/valyn/.nixos";
 }
