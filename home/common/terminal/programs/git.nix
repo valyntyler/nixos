b33a01ccbd5dafb82a140b3fff499b280e210f4e
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-28 18:08:16 +0100

    fix: update git.nix

--- home/common/terminal/programs/git.nix
+++ home/common/terminal/programs/git.nix
@@ -2,10 +2,12 @@
   programs.git = {
     enable = true;
     lfs.enable = true;
-    settings.init.defaultBranch = "main";
-    user = {
-      name = "Valyn Tyler";
-      email = "115489989+ValynTyler@users.noreply.github.com";
+    settings = {
+      init.defaultBranch = "main";
+      user = {
+        name = "Valyn Tyler";
+        email = "115489989+ValynTyler@users.noreply.github.com";
+      };
     };
   };
 }
