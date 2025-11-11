6f4484df0c36b6d87ab5d12b968b5f9f5133b7de
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-11 20:25:59 +0100

    chore: update frame/default

--- host/frame12/default.nix
+++ host/frame12/default.nix
@@ -5,15 +5,14 @@
     ../common/optional/gnome.nix
     ../common/optional/home-manager.nix
     ../common/optional/xdg.nix
+    ../common/optional/hardware/graphics.nix
     ../common/optional/programs/discord.nix
     ../common/optional/programs/nix-ld.nix
     ../common/optional/programs/spotify.nix
     ../common/optional/programs/steam.nix
     ../common/optional/services/kanata.nix
     ../common/optional/services/mullvad.nix
-    ../common/optional/hardware/graphics.nix
   ];
 
-  # Initial NixOS version
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.11";
 }
