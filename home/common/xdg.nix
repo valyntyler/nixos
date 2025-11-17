9184f111e2c87b25e98b1b504f8a545a3ad89d44
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-17 22:54:33 +0100

    fix: xdg browser defaults

--- home/common/xdg.nix
+++ home/common/xdg.nix
@@ -19,8 +19,11 @@
     mimeApps = {
       enable = true;
       defaultApplications = {
+        "text/html" = "firefox.desktop";
         "x-scheme-handler/http" = ["firefox.desktop"];
         "x-scheme-handler/https" = ["firefox.desktop"];
+        "x-scheme-handler/about" = "firefox.desktop";
+        "x-scheme-handler/unknown" = "firefox.desktop";
         "text/plain" = ["org.gnome.TextEditor.desktop"];
         "image/jpeg" = ["org.gnome.Loupe.desktop"];
         "image/jpg" = ["org.gnome.Loupe.desktop"];
