a79b97c111b5d0bcb4bde0ecec515ef4e1c5b91f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-17 18:29:05 +0100

    chore: remove spotifyd.nix

--- host/common/optional/services/spotifyd.nix
+++
@@ -1,6 +0,0 @@
-{pkgs, ...}: {
-  services.spotifyd.enable = true;
-  networking.firewall.allowedTCPPorts = [5353];
-  networking.firewall.allowedUDPPorts = [5353];
-  environment.systemPackages = with pkgs; [spotifyd];
-}
