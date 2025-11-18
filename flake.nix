23b63a374fdb4955cd6a03c484ea798bc2fc93bc
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-18 13:58:14 +0100

    chore: remove nixpkgs-stable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/master";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
