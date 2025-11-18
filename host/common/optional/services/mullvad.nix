fe9bb55b27e211dde558958576ef802e6c1e3cae
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-11-18 15:22:33 +0100

    fix: mullvad-vpn

--- host/common/optional/services/mullvad.nix
+++ host/common/optional/services/mullvad.nix
@@ -1,3 +1,9 @@
-{
-  services.mullvad-vpn.enable = false;
+{inputs, ...}: let
+  pkgs-stable = inputs.nixpkgs-stable.legacyPackages.x86_64-linux;
+in {
+  environment.systemPackages = with pkgs-stable; [mullvad-vpn];
+  services.mullvad-vpn = {
+    enable = true;
+    package = pkgs-stable.mullvad-vpn;
+  };
 }
