a648031f01a8eabfdb57a78e0ab9299a322546cd
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-12-05 17:31:11 +0100

    fix: temp clangd config

--- home/valyn/default.nix
+++ home/valyn/default.nix
@@ -22,6 +22,18 @@
     inputs.zen-browser.homeModules.beta
   ];
 
+  # FIX: clangd can't find libgcc on its own for now
+  home.file.".config/clangd/config.yaml".text = ''
+    CompileFlags:
+      Add:
+        - "-I/nix/store/gi4cz4ir3zlwhf1azqfgxqdnczfrwsr7-glibc-2.40-66-dev/include"
+        - "-I/nix/store/gj06hl0vb46kf66zpsss4gk5wphfh5lk-compiler-rt-libc-21.1.2-dev/include"
+        - "-I/nix/store/kzq78n13l8w24jn8bx4djj79k5j717f1-gcc-14.3.0/include/c++/14.3.0"
+        - "-I/nix/store/kzq78n13l8w24jn8bx4djj79k5j717f1-gcc-14.3.0/include/c++/14.3.0/x86_64-unknown-linux-gnu"
+        - "-I/nix/store/sqlnjj8c3n3si3sjnadhdbcwgrk97g2w-clang-wrapper-21.1.2/resource-root/include"
+        - "-I/nix/store/gi4cz4ir3zlwhf1azqfgxqdnczfrwsr7-glibc-2.40-66-dev/include"
+  '';
+
   home = {
     username = "valyn";
     homeDirectory = "/home/valyn";
