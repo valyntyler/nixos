e54e3953789bc3946e79ea796ac1d00bf332033d
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-12-18 19:48:07 +0100

    fix: openssh user

--- host/common/core/services/openssh.nix
+++ host/common/core/services/openssh.nix
@@ -1,4 +1,4 @@
-{
+{user, ...}: {
   services.openssh = {
     enable = true;
     ports = [22];
@@ -10,7 +10,7 @@
       PermitRootLogin = "prohibit-password";
     };
   };
-  users.users.root.openssh.authorizedKeys.keys = [
+  users.users.${user}.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGIVxfTlZj47zMoLfBLVJgSXpsPE1h/9xSjs2hP31m7n valyn@desktop"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPpDGLs0LclHmsIzpXzLuvf7Ptyp7OEGsIrr2g/ZpzNL valyn@frame12"
   ];
