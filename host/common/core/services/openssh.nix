9e7f4d46caf33c322460015a795720f933a44b5a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-12-18 19:40:19 +0100

    fix: desktop ssh key

--- host/common/core/services/openssh.nix
+++ host/common/core/services/openssh.nix
@@ -11,6 +11,7 @@
     };
   };
   users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGIVxfTlZj47zMoLfBLVJgSXpsPE1h/9xSjs2hP31m7n valyn@desktop"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPpDGLs0LclHmsIzpXzLuvf7Ptyp7OEGsIrr2g/ZpzNL valyn@frame12"
   ];
 }
