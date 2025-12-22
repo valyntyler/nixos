acf78fee03c95d8c72980aee7e60e6e4f3be8b62
Valyn Tyler <git@valyntyler.com>
2025-12-23 00:35:31 +0200

    chore: remove mullvad.nix

--- host/common/optional/services/mullvad.nix
+++
@@ -1,9 +0,0 @@
-{inputs, ...}: let
-  pkgs-stable = inputs.nixpkgs-stable.legacyPackages.x86_64-linux;
-in {
-  environment.systemPackages = with pkgs-stable; [mullvad-vpn];
-  services.mullvad-vpn = {
-    enable = true;
-    package = pkgs-stable.mullvad-vpn;
-  };
-}
