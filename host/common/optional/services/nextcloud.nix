717f8ab4edaa058a147852026d1ff3c81e03a99f
Valyn Tyler <git@valyntyler.com>
2025-12-24 03:23:40 +0200

    feat: complete nextcloud config

--- host/common/optional/services/nextcloud.nix
+++ host/common/optional/services/nextcloud.nix
@@ -8,11 +8,15 @@
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud32;
-    hostName = "localhost";
+    hostName = "cloud.valyntyler.com";
     config = {
       dbtype = "sqlite";
       adminuser = "root";
       adminpassFile = config.sops.secrets."nextcloud/admin/pass".path;
     };
+  };
+  services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+    forceSSL = true;
+    enableACME = true;
   };
 }
