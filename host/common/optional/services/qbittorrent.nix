f3c6f680e6caad644b74bf5a019a4f00fbc3dafc
Valyn Tyler <git@valyntyler.com>
2026-01-03 05:38:18 +0200

    feat: qbitwebui service url

--- host/common/optional/services/qbittorrent.nix
+++ host/common/optional/services/qbittorrent.nix
@@ -8,4 +8,9 @@
       RootFolder = "${inputs.qbitwebui.packages.x86_64-linux.default}/share/qbitwebui";
     };
   };
+  services.nginx.virtualHosts."torrent.valyntyler.com" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/".proxyPass = "http://127.0.0.1:8181";
+  };
 }
