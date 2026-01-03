49bc4392073bbd047fca3ec07849f721c04ea088
Valyn Tyler <git@valyntyler.com>
2026-01-03 23:50:14 +0200

    fix: back to qbittorrent regular

--- host/common/optional/services/qbittorrent.nix
+++ host/common/optional/services/qbittorrent.nix
@@ -1,4 +1,5 @@
-{inputs, ...}: {
+# {inputs, ...}:
+{
   services.qbittorrent = {
     enable = true;
     openFirewall = true;
@@ -8,34 +9,40 @@
       Password_PBKDF2 = "@ByteArray(Dn9j11tFgSkcUQOY6WS8SA==:vbeQ/SP6v6GcJKKpUylpVm/zcFm4nNPXU5goV/IFzkhuwaTeA3vJG1LCvzanRt9Ohv5WF7ewayBZMNCj0pBgEQ==)";
     };
   };
-  users.groups.qbitwebui = {};
-  users.users.qbitwebui = {
-    home = "/var/lib/qbitwebui";
-    createHome = true;
-    isSystemUser = true;
-    group = "qbitwebui";
-  };
-  systemd.tmpfiles.rules = [
-    "d /var/lib/qbitwebui 0777 qbitwebui qbitwebui  -"
-  ];
-  systemd.services.qbitwebui = {
-    wantedBy = ["multi-user.target"];
-    environment = {
-      DATABASE_PATH = "/var/lib/qbitwebui";
-      SALT_PATH = "/var/lib/qbitwebui";
-      PORT = "8182";
-    };
-    serviceConfig = {
-      # EnvironmentFile = "/run/secrets/myenv";
-      ExecStart = "${inputs.qbitwebui.packages.x86_64-linux.default}/bin/qbitwebui";
-      StateDirectory = "/var/lib/qbitwebui";
-      User = "qbitwebui";
-      Group = "qbitwebui";
-    };
-  };
+  # users.groups.qbitwebui = {};
+  # users.users.qbitwebui = {
+  #   home = "/var/lib/qbitwebui";
+  #   createHome = true;
+  #   isSystemUser = true;
+  #   group = "qbitwebui";
+  # };
+  # systemd.tmpfiles.rules = [
+  #   "d /var/lib/qbitwebui 0777 qbitwebui qbitwebui  -"
+  # ];
+  # systemd.services.qbitwebui = {
+  #   wantedBy = ["multi-user.target"];
+  #   environment = {
+  #     DATABASE_PATH = "/var/lib/qbitwebui";
+  #     SALT_PATH = "/var/lib/qbitwebui";
+  #     PORT = "8182";
+  #   };
+  #   serviceConfig = {
+  #     # EnvironmentFile = "/run/secrets/myenv";
+  #     ExecStart = "${inputs.qbitwebui.packages.x86_64-linux.default}/bin/qbitwebui";
+  #     StateDirectory = "/var/lib/qbitwebui";
+  #     User = "qbitwebui";
+  #     Group = "qbitwebui";
+  #   };
+  # };
   services.nginx.virtualHosts."torrent.valyntyler.com" = {
     forceSSL = true;
     enableACME = true;
-    locations."/".proxyPass = "http://127.0.0.1:8182";
+    locations."/".proxyPass = "http://127.0.0.1:8181";
+    locations."/".extraConfig = ''
+      proxy_set_header Host $host;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto $scheme;
+    '';
   };
 }
