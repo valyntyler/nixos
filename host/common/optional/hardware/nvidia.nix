34bcf9078ee650546ef73273e82649f1e15775df
Valyn Tyler <git@valyntyler.com>
2026-01-18 21:34:16 +0100

    fix: update nvidia.nix

--- host/common/optional/hardware/nvidia.nix
+++ host/common/optional/hardware/nvidia.nix
@@ -1,7 +1,8 @@
 {
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = ["nvidia" "modesetting"];
   hardware.nvidia = {
     open = true;
     nvidiaSettings = false;
+    modesetting.enable = true;
   };
 }
