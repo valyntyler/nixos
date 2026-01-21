96f43f072061c2a2d53b97f9837b3388bd28fa36
Valyn Tyler <git@valyntyler.com>
2026-01-21 23:45:37 +0100

    fix: revert intellij to stable

--- home/common/software/editor/intellij.nix
+++ home/common/software/editor/intellij.nix
@@ -1,3 +1,5 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [jetbrains.idea-oss];
+{inputs, ...}: let
+  pkgs = inputs.nixpkgs-stable.legacyPackages.x86_64-linux;
+in {
+  home.packages = with pkgs; [jetbrains.idea-community];
 }
