f07459d747af98f22b6173c67e167ad99302c209
Valyn Tyler <git@valyntyler.com>
2026-02-03 17:54:16 +0100

    fix: termshark

--- host/common/optional/programs/wireshark.nix
+++ host/common/optional/programs/wireshark.nix
@@ -1,5 +1,12 @@
-{pkgs, ...}: {
-  users.users."valyn".extraGroups = ["wireshark"];
+{
+  pkgs,
+  user,
+  ...
+}: {
+  users.users.${user}.extraGroups = ["wireshark"];
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usbmon", GROUP="wireshark", MODE="0640"
+  '';
   environment.systemPackages = with pkgs; [
     termshark
     wireshark
