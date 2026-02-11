1187b584ea98e904a02b7b5d14a0d99248c45494
Valyn Tyler <git@valyntyler.com>
2026-02-12 00:30:55 +0100

    chore: remove nginx

--- host/desktop/default.nix
+++ host/desktop/default.nix
@@ -20,7 +20,6 @@
     ../common/optional/services/jellyfin.nix
     ../common/optional/services/minecraft.nix
     # ../common/optional/services/nextcloud.nix
-    ../common/optional/services/nginx.nix
     ../common/optional/services/qbittorrent.nix
   ];
 
