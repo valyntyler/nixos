1f8e2bdcf578fba79bd563322822ec672e161074
Valyn Tyler <git@valyntyler.com>
2026-02-12 00:47:22 +0100

    fix: update onlyoffice.nix

--- host/common/optional/services/onlyoffice.nix
+++ host/common/optional/services/onlyoffice.nix
@@ -1,6 +1,8 @@
 {pkgs, ...}: {
   services.onlyoffice = {
     enable = true;
+    wopi = true;
+    port = 8441;
     hostname = "localhost";
     securityNonceFile = "${pkgs.writeText "nixos-test-onlyoffice-nonce.conf" ''
       set $secure_link_secret "nixostest";
