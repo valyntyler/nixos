8da8792e2d053995f52647727c9768387fc2e9ed
Valyn Tyler <git@valyntyler.com>
2026-02-12 01:28:00 +0100

    fix: update onlyoffice.nix

--- host/common/optional/services/onlyoffice.nix
+++ host/common/optional/services/onlyoffice.nix
@@ -1,11 +1,18 @@
 {pkgs, ...}: {
   services.onlyoffice = {
     enable = true;
-    wopi = true;
-    port = 8441;
-    hostname = "localhost";
+    # wopi = true;
+    port = 8111;
+    hostname = "office.valyntyler.com";
     securityNonceFile = "${pkgs.writeText "nixos-test-onlyoffice-nonce.conf" ''
       set $secure_link_secret "nixostest";
     ''}";
   };
+  services.nginx.virtualHosts."office.valyntyler.com" = {
+    forceSSL = true;
+    enableACME = true;
+  };
+  system.activationScripts.onlyoffice-readable.text = ''
+    chmod a+x /var/lib/onlyoffice/documentserver/
+  '';
 }
