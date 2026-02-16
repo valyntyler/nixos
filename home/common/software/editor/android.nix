fc5c9bcac756974ec47f624b722c836fa0de6e95
Valyn Tyler <git@valyntyler.com>
2026-02-16 12:06:44 +0100

    chore: moved android studio to stale pkgs

--- home/common/software/editor/android.nix
+++ home/common/software/editor/android.nix
@@ -1,3 +1,12 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [android-studio];
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  pkgsStale = import inputs.nixpkgs-stable {
+    inherit (pkgs.stdenv.hostPlatform) system;
+    config.allowUnfree = true;
+  };
+in {
+  home.packages = with pkgsStale; [android-studio];
 }
