1cfd06c3b63ee2cd0878ff0df7d6aa9ae7b69cf0
Valyn Tyler <git@valyntyler.com>
2026-02-16 12:02:48 +0100

    chore: adopt new xorg naming scheme

--- host/common/optional/programs/nix-ld.nix
+++ host/common/optional/programs/nix-ld.nix
@@ -87,32 +87,32 @@
       udev
       vulkan-loader
       wayland
-      xorg.libICE
-      xorg.libSM
-      xorg.libX11
-      xorg.libXScrnSaver
-      xorg.libXcomposite
-      xorg.libXcursor
-      xorg.libXdamage
-      xorg.libXext
-      xorg.libXfixes
-      xorg.libXft
-      xorg.libXi
-      xorg.libXinerama
-      xorg.libXmu
-      xorg.libXrandr
-      xorg.libXrender
-      xorg.libXt
-      xorg.libXtst
-      xorg.libXxf86vm
-      xorg.libpciaccess
-      xorg.libxcb
-      xorg.xcbutil
-      xorg.xcbutilimage
-      xorg.xcbutilkeysyms
-      xorg.xcbutilrenderutil
-      xorg.xcbutilwm
-      xorg.xkeyboardconfig
+      libice
+      libsm
+      libx11
+      libxscrnsaver
+      libxcomposite
+      libxcursor
+      libxdamage
+      libxext
+      libxfixes
+      libxft
+      libxi
+      libxinerama
+      libxmu
+      libxrandr
+      libxrender
+      libxt
+      libxtst
+      libxxf86vm
+      libpciaccess
+      libxcb
+      xcbutil
+      xcbutilimage
+      xcbutilkeysyms
+      xcbutilrenderutil
+      xcbutilwm
+      xkeyboardconfig
       xz
       zlib
     ];
