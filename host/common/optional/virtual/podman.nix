2543498dc62aaeb6c5417f90a1877b0a054f798d
Valyn Tyler <git@valyntyler.com>
2026-02-23 19:34:01 +0100

    fix: disable podman dockercompat

--- host/common/optional/virtual/podman.nix
+++ host/common/optional/virtual/podman.nix
@@ -3,7 +3,7 @@
   virtualisation.containers.enable = true;
   virtualisation.podman = {
     enable = true;
-    dockerCompat = true;
+    # dockerCompat = true;
     defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
   };
 }
