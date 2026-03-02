ba48faf5cce00dceb72f89094ee5a8fdff8c0e37
Valyn Tyler <git@valyntyler.com>
2026-03-02 11:00:20 +0100

    feat: update git.nix

--- home/common/terminal/programs/git.nix
+++ home/common/terminal/programs/git.nix
@@ -4,6 +4,7 @@
     lfs.enable = true;
     settings = {
       init.defaultBranch = "main";
+      push.autoSetupRemote = true;
       user = {
         name = "Valyn Tyler";
         email = "git@valyntyler.com";
