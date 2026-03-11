94f7e222d88349dfa44b82bfd3e7b09cfdc23851
Valyn Tyler <git@valyntyler.com>
2026-03-11 15:02:04 +0100

    fix: disable xwayland scaling

--- host/common/optional/de/gnome/experimental.nix
+++ host/common/optional/de/gnome/experimental.nix
@@ -6,7 +6,6 @@
           experimental-features = [
             "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
             "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
-            "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
           ];
         };
       };
