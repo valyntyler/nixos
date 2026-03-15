3c2debea4f090f5be0d7c18497349316f92740a6
Valyn Tyler <git@valyntyler.com>
2026-03-15 18:11:47 +0100

    chore: remove nu keybind

--- home/common/terminal/shell/nushell/settings.nix
+++ home/common/terminal/shell/nushell/settings.nix
@@ -5,21 +5,5 @@
       enable = true;
       max_results = 200;
     };
-    keybindings = [
-      {
-        name = "completion_menu";
-        modifier = "control";
-        keycode = "char_l";
-        mode = "emacs";
-        event = [
-          {edit = "Clear";}
-          {
-            edit = "InsertString";
-            value = "clear";
-          }
-          {send = "Enter";}
-        ];
-      }
-    ];
   };
 }
