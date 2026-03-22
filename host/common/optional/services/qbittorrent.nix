d9d3454ea00dc36b93cacce8aecbaafbb4a64335
Valyn Tyler <git@valyntyler.com>
2026-03-22 22:04:47 +0100

    chore: update qbittorrent.nix

--- host/common/optional/services/qbittorrent.nix
+++ host/common/optional/services/qbittorrent.nix
@@ -1,6 +1,6 @@
-# {inputs, ...}:
-{
+{user, ...}: {
   services.qbittorrent = {
+    inherit user;
     enable = true;
     openFirewall = true;
     webuiPort = 8181;
@@ -9,31 +9,6 @@
       Password_PBKDF2 = "@ByteArray(Dn9j11tFgSkcUQOY6WS8SA==:vbeQ/SP6v6GcJKKpUylpVm/zcFm4nNPXU5goV/IFzkhuwaTeA3vJG1LCvzanRt9Ohv5WF7ewayBZMNCj0pBgEQ==)";
     };
   };
-  # users.groups.qbitwebui = {};
-  # users.users.qbitwebui = {
-  #   home = "/var/lib/qbitwebui";
-  #   createHome = true;
-  #   isSystemUser = true;
-  #   group = "qbitwebui";
-  # };
-  # systemd.tmpfiles.rules = [
-  #   "d /var/lib/qbitwebui 0777 qbitwebui qbitwebui  -"
-  # ];
-  # systemd.services.qbitwebui = {
-  #   wantedBy = ["multi-user.target"];
-  #   environment = {
-  #     DATABASE_PATH = "/var/lib/qbitwebui";
-  #     SALT_PATH = "/var/lib/qbitwebui";
-  #     PORT = "8182";
-  #   };
-  #   serviceConfig = {
-  #     # EnvironmentFile = "/run/secrets/myenv";
-  #     ExecStart = "${inputs.qbitwebui.packages.x86_64-linux.default}/bin/qbitwebui";
-  #     StateDirectory = "/var/lib/qbitwebui";
-  #     User = "qbitwebui";
-  #     Group = "qbitwebui";
-  #   };
-  # };
   services.nginx.virtualHosts."torrent.valyntyler.com" = {
     forceSSL = true;
     enableACME = true;
