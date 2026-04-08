5a0430d7edbff185700744fcae89aac3405468e2
Valyn Tyler <git@valyntyler.com>
2026-04-08 15:36:17 +0200

    chore: sort mime types

--- home/common/xdg.nix
+++ home/common/xdg.nix
@@ -20,21 +20,21 @@
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "text/html" = ["firefox.desktop"];
-        "text/markdown" = ["org.gnome.gitlab.somas.Apostrophe.desktop"];
-        "x-scheme-handler/http" = ["firefox.desktop"];
-        "x-scheme-handler/https" = ["firefox.desktop"];
-        "x-scheme-handler/about" = "firefox.desktop";
-        "x-scheme-handler/unknown" = "firefox.desktop";
-        "text/plain" = ["org.gnome.TextEditor.desktop"];
+        "application/json" = ["org.gnome.TextEditor.desktop"];
+        "application/pdf" = ["org.gnome.Papers.desktop"];
+        "application/zip" = ["org.gnome.FileRoller.desktop"];
         "image/jpeg" = ["org.gnome.Loupe.desktop"];
         "image/jpg" = ["org.gnome.Loupe.desktop"];
         "image/png" = ["org.gnome.Loupe.desktop"];
         "image/svg+xml" = ["org.inkscape.Inkscape.desktop"];
+        "text/html" = ["firefox.desktop"];
+        "text/markdown" = ["org.gnome.gitlab.somas.Apostrophe.desktop"];
+        "text/plain" = ["org.gnome.TextEditor.desktop"];
         "video/webm" = ["org.gnome.Showtime.desktop"];
-        "application/pdf" = ["org.gnome.Papers.desktop"];
-        "application/zip" = ["org.gnome.FileRoller.desktop"];
-        "application/json" = ["org.gnome.TextEditor.desktop"];
+        "x-scheme-handler/about" = "firefox.desktop";
+        "x-scheme-handler/http" = ["firefox.desktop"];
+        "x-scheme-handler/https" = ["firefox.desktop"];
+        "x-scheme-handler/unknown" = "firefox.desktop";
       };
     };
   };
