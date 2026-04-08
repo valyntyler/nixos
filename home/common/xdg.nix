3e4f420407cb72d1e402075371a6fb8c67f74958
Valyn Tyler <git@valyntyler.com>
2026-04-08 15:38:01 +0200

    refactor: cleaned up desktopEntries expression

--- home/common/xdg.nix
+++ home/common/xdg.nix
@@ -2,13 +2,13 @@
   xdg = {
     enable = true;
     autostart.enable = true;
-    desktopEntries = let
-      value = {
-        name = "";
-        noDisplay = true;
-      };
-    in
-      builtins.listToAttrs (map (name: {inherit name value;}) [
+    desktopEntries = builtins.listToAttrs (map (name: {
+        inherit name;
+        value = {
+          name = "";
+          noDisplay = true;
+        };
+      }) [
         "aerc"
         "btop"
         "fish"
