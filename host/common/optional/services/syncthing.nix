45ba5361634f6a8b0e825016caab42a16a4f2308
Valyn Tyler <git@valyntyler.com>
2026-04-28 13:44:17 +0200

    feat: add ideapad id

--- host/common/optional/services/syncthing.nix
+++ host/common/optional/services/syncthing.nix
@@ -6,6 +6,7 @@
     settings = {
       devices = {
         "frame12" = {id = "KJCYSJQ-NUDVKLV-PQ7IHR6-TMSHFXZ-DCIH5OG-B5H34E7-PCVRFGA-CKAY5AD";};
+        "ideapad" = {id = "MOYZP2T-QSTKIKY-FRBXBCL-GAQJRMT-XNBDS7J-DA3LPN3-OAMBFP2-G5HMTA5";};
       };
       folders = {
         "Notes" = {
