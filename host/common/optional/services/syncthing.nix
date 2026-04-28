8640e40d41ca5da64d67052bc1285436a125c700
Valyn Tyler <git@valyntyler.com>
2026-04-28 14:10:34 +0200

    fix: ideapad id

--- host/common/optional/services/syncthing.nix
+++ host/common/optional/services/syncthing.nix
@@ -9,7 +9,7 @@
     settings = {
       devices = {
         "frame12" = {id = "KJCYSJQ-NUDVKLV-PQ7IHR6-TMSHFXZ-DCIH5OG-B5H34E7-PCVRFGA-CKAY5AD";};
-        "ideapad" = {id = "MOYZP2T-QSTKIKY-FRBXBCL-GAQJRMT-XNBDS7J-DA3LPN3-OAMBFP2-G5HMTA5";};
+        "ideapad" = {id = "CHJ24Z2-ADITCXI-3BABDI6-22CS47T-YD5W63U-HB34QVX-KOMWWQ6-I3XVLAH";};
       };
       folders = {
         "Notes" = {
