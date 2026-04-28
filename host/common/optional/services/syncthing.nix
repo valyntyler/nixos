be8d630e5fa6b197306c64371623aad580eb90a6
Valyn Tyler <git@valyntyler.com>
2026-04-28 13:36:03 +0200

    feat: update syncthing

--- host/common/optional/services/syncthing.nix
+++ host/common/optional/services/syncthing.nix
@@ -1,8 +1,24 @@
-{
+{user, ...}: {
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
     # guiAddress = "0.0.0.0:8384";
+    settings = {
+      # gui.user = "myuser";
+      # devices = {
+      #   "device1" = {id = "DEVICE-ID-GOES-HERE";};
+      #   "device2" = {id = "DEVICE-ID-GOES-HERE";};
+      # };
+      folders = {
+        "Notes" = {
+          path = "/home/${user}/Notes";
+          devices = [
+            # "device1"
+            # "device2"
+          ];
+        };
+      };
+    };
   };
   networking.firewall.allowedTCPPorts = [8384];
 }
