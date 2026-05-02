371110e70a6b6f52ed4c8c59f3b00a176ca348a5
Valyn Tyler <git@valyntyler.com>
2026-05-02 21:56:19 +0200

    temp: disable wireshark

--- host/common/optional/programs/wireshark.nix
+++ host/common/optional/programs/wireshark.nix
@@ -3,17 +3,17 @@
   user,
   ...
 }: {
-  users.users.${user}.extraGroups = ["wireshark"];
-  services.udev.extraRules = ''
-    SUBSYSTEM=="usbmon", GROUP="wireshark", MODE="0640"
-  '';
+  # users.users.${user}.extraGroups = ["wireshark"];
+  # services.udev.extraRules = ''
+  #   SUBSYSTEM=="usbmon", GROUP="wireshark", MODE="0640"
+  # '';
   environment.systemPackages = with pkgs; [
     # termshark
-    wireshark
+    # wireshark
   ];
-  programs.wireshark = {
-    enable = true;
-    dumpcap.enable = true;
-    usbmon.enable = true;
-  };
+  # programs.wireshark = {
+  #   enable = true;
+  #   dumpcap.enable = true;
+  #   usbmon.enable = true;
+  # };
 }
